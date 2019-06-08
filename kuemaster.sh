b61469a54c13633bd0945c729ec4c92440ff0ee9
jira <k.naree@gmail.com>
2019-06-08 00:55:57 -0400

    adding kuber setup files
    these files are to setup kubernetes cluster in centos

---
+++ kuemaster.sh
@@ -0,0 +1,70 @@
+#Disable SELinux
+setenforce 0
+sed -i --follow-symlinks 's/SELINUX=enforcing/SELINUX=disabled/g' /etc/sysconfig/selinux
+
+#Enable the br_netfilter module for cluster communication
+modprobe br_netfilter
+echo '1' > /proc/sys/net/bridge/bridge-nf-call-iptables
+
+#Disable swap to prevent memory allocation issues
+swapoff -a
+#vim /etc/fstab.orig  ->  Comment out the swap line
+sudo sed -i 's/.*swap/#&/' /etc/fstab.orig
+
+#Install the Docker prerequisites
+yum install -y yum-utils device-mapper-persistent-data lvm2
+
+
+#Add the Docker repo and install Docker
+yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+yum install -y docker-ce
+
+
+#Conigure the Docker Cgroup Driver to systemd, enable and start Docker
+sed -i '/^ExecStart/ s/$/ --exec-opt native.cgroupdriver=systemd/' /usr/lib/systemd/system/docker.service 
+systemctl daemon-reload
+systemctl enable docker --now
+
+#Add the Kubernetes repo
+cat <<EOF > /etc/yum.repos.d/kubernetes.repo
+[kubernetes]
+name=Kubernetes
+baseurl=https://packages.cloud.google.com/yum/repos/kubernetes-el7-x86_64
+enabled=1
+gpgcheck=0
+repo_gpgcheck=0
+gpgkey=https://packages.cloud.google.com/yum/doc/yum-key.gpg
+https://packages.cloud.google.com/yum/doc/rpm-package-key.gpg
+EOF
+
+#Install Kubernetes
+yum install -y kubelet kubeadm kubectl
+
+
+#Enable Kubernetes. The kubelet service will not start until you run kubeadm init
+systemctl enable kubelet
+
+
+#For  KubeMaster run these extra steps
+
+
+#Initialize the cluster using the IP range for Flannel
+kubeadm init --pod-network-cidr=10.244.0.0/16
+
+exit
+#to run Kube without root
+mkdir -p $HOME/.kube
+sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+sudo chown $(id -u):$(id -g) $HOME/.kube/config
+
+#For  KubeMaster run these extra steps
+
+
+#Initialize the cluster using the IP range for Flannel
+kubeadm init --pod-network-cidr=10.244.0.0/16
+
+
+#to run Kube without root
+mkdir -p $HOME/.kube
+sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+sudo chown $(id -u):$(id -g) $HOME/.kube/config
